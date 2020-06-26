d02baeb8bd5cb30d8fb2be4d31d2185ec9f575e5
Ivan <I.Dimitrov@student.tudelft.nl>
2020-06-26 11:59:42 +0300

    Adding some unadded stuuf

---
+++ ADS/src/prep/BTComplete.java
@@ -0,0 +1,20 @@
+//package prep;
+//
+//import DataStructures.SinglyLists.Node;
+//import DataStructures.SinglyLists.SinglyLinkedList;
+//import DataStructures.Trees.BinaryTree;
+//
+//import java.util.LinkedList;
+//
+//public class BTComplete {
+//
+//    public static boolean isTreeComplete(BinaryTree tree) {
+//        if(tree == null) return true;
+//        Queue<BinaryTree> q = (Queue<BinaryTree>) new LinkedList<BinaryTree>();
+//        q.enqueue(tree);
+//        while(!q.isEmpty()) {
+//            BinaryTree tr = q.poll();
+//        }
+//    }
+//
+//} 
