b3150f0f5ab6517dc3419ff4bb0c18a6c776f5aa
Ivan <I.Dimitrov@student.tudelft.nl>
2020-06-26 12:04:14 +0300

    unadded stuff

---
+++ ADS/src/prep/MyQueue.java
@@ -0,0 +1,37 @@
+package prep;
+
+import DataStructures.SinglyLists.SinglyLinkedList;
+
+
+public class MyQueue extends SinglyLinkedList<String> {
+
+    SinglyLinkedList<String> list = new SinglyLinkedList<>();
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void enqueue(String a) {
+        list.addFirst(a);
+    }
+
+
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enqueue("asd");
+        queue.enqueue("adf");
+        queue.enqueue("sad");
+        queue.enqueue("qw");
+        DataStructures.SinglyLists.Node node = queue.head;
+        for(int i = 0;i < queue.size() - 1;i++) {
+            System.out.println(node.toString());
+            node = node.getNext();
+        }
+    }
+}
+
+
